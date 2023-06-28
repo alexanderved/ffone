b1d77740d253e691c3087e2f649d166c1819d875
alexanderved <avedehin692@gmail.com>
2023-06-28 18:47:52 +0300

    Move repeating behaviour into Component And Runnable traits

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,4 +2,31 @@ pub extern crate mueue;
 
 pub mod audio_system;
 pub mod controller;
+pub mod device_link;
 pub mod view;
+
+use std::sync::Arc;
+
+use mueue::{Message, MessageEndpoint};
+
+pub trait Component {
+    type Message: Message;
+    type ControlMessage: Message;
+
+    fn endpoint(&self) -> MessageEndpoint<Self::ControlMessage, Self::Message>;
+    fn connect(&mut self, end: MessageEndpoint<Self::ControlMessage, Self::Message>);
+
+    fn send(&self, msg: Self::Message) {
+        let _ = self.endpoint().send(Arc::new(msg));
+    }
+}
+
+pub trait Runnable {
+    fn update(&mut self);
+
+    fn run(&mut self) {
+        loop {
+            self.update();
+        }
+    }
+}
